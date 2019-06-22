dd8a643a53bdc7bf37b547e7e8dd306c1b800afb
t-site <sktruntime@hotmail.co.jp>
2019-06-22 09:18:23 +0900

    random number buffer for less /dev/urandom IO context.

--- quantum_interpole.c
+++ quantum_interpole.c
@@ -29,11 +29,14 @@ SOFTWARE.
 #define MAX 256
 #define COLORS 3
 #define ITER 5
+#define RA_SIZE 4194304
 
 static int input_pixels[COLORS][KNL][KNL];
 static int output_pixels[COLORS][KNL][KNL];
 static int mse[COLORS][HALF_KNL][HALF_KNL];
 static int t=1;
+static int randpointer=0;
+static signed char randarray[RA_SIZE];
 static unsigned int imageSX;
 static unsigned int imageSY;
 static FILE *randomfd;
@@ -80,6 +83,26 @@ static long half_psnr_mse(void)
 	return all;
 }
 
+static signed char getrand(void)
+{
+	signed char rand;
+	if(randpointer == 0)
+	{
+		fread(randarray,sizeof(signed char),RA_SIZE,randomfd);
+	}
+	rand = randarray[randpointer];
+	if( randpointer >= RA_SIZE )
+	{
+		randpointer = 0;
+	}
+	else
+	{
+		randpointer++;
+	}
+	return rand;
+
+}
+
 static int quantum_art( void )
 {
 	unsigned int x,y,c;
@@ -93,15 +116,15 @@ static int quantum_art( void )
 			{
 				for ( c=0 ; c<COLORS ; c++)
 				{
-	
 					signed char rand;
-					counter++;
-					rand = 0xFF & fgetc(randomfd);
+					rand = getrand();
 					output_pixels[c][y][x] += (int)rand % ( mse[c][y/2][x/2] /2 ) ;
+					counter++;
 					if(output_pixels[c][y][x] >= MAX )
 						output_pixels[c][y][x] = MAX-1 ;
 					else if(output_pixels[c][y][x] < 0 )
 						output_pixels[c][y][x] = 0 ;
+					
 				}
 			}
 		}
