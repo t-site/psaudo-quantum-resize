80892e29cd654a03a386b13e900b5576a5b07cab
t-site <sktruntime@hotmail.co.jp>
2019-07-07 14:05:57 +0900

    correct copyright year

--- quantum_interpole.c
+++ quantum_interpole.c
@@ -1,5 +1,5 @@
 /*
-Copyright (c) 2018 "tropposite" as on https://github.com/t-site
+Copyright (c) 2019 "tropposite" as on https://github.com/t-site
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
