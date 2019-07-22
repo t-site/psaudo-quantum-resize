a8871bc67ba10fef0012c616a9b535a182036ec7
t-site <sktruntime@hotmail.co.jp>
2019-07-22 19:55:37 +0900

    build.sh correct build error

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 rm qresize qresize1.4
-gcc -g main.c quantum_interpole.c `pkg-config gdlib --libs --cflags` -fstack-protector-strong -Wall -o qresize
+gcc -g main.c divide.c quantum_interpole.c `pkg-config gdlib --libs --cflags` -fstack-protector-strong -Wall -o qresize
 gcc -g qresize-1.4.c quantum_interpole-1.4.c `pkg-config gdlib --libs --cflags` -fstack-protector-strong -Wall -o qresize1.4
